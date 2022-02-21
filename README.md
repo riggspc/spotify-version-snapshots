2096ddce05dac1e813f4435519c523016f253196
Patrick Riggs <riggspc@gmail.com>
2022-02-21 13:51:32 -0800

    Add method to fetch saved albums
    Fetch current user saved albums in a manner similar to saved songs. Copypasta a bunch of code to do it. As such, clarify in readme that this is a hacky project until it's not anymore

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Take snapshots of the state of your Spotify library/playlists/etc. Will
 eventually be able to output these snapshots in a format understood by version
 control, essentially giving you to ability to track your Spotify world as it
 evolves over time.
+
+*This code is hacky, maybe it'll be cleaned up later*
--- main.py
+++ main.py
@@ -5,8 +5,11 @@ from spotipy.oauth2 import SpotifyOAuth
 from spotipy import Spotify
 from credentials import CLIENT_ID, CLIENT_SECRET
 
+API_REQUEST_SLEEP_TIME_SEC = 0.5
+
 # General TODOs:
 # - Error handling
+# - Refactor fetch code to share more logic
 
 
 def get_saved_tracks(sp_client: Spotify) -> dict:
@@ -18,7 +21,7 @@ def get_saved_tracks(sp_client: Spotify) -> dict:
     while True:
         results = sp_client.current_user_saved_tracks(limit, offset)
         result_items = results["items"]
-        print(f"Fetched {len(result_items)} items")
+        print(f"Fetched {len(result_items)} tracks")
 
         # result_items is a list. Add them to the saved_tracks dict by track ID
         # to prevent duplicated tracks appearing if the library was dded to
@@ -31,9 +34,35 @@ def get_saved_tracks(sp_client: Spotify) -> dict:
         offset += limit
 
         # Prevent rate limiting. Maybe not needed, playing it safe for now
-        time.sleep(0.5)
+        time.sleep(API_REQUEST_SLEEP_TIME_SEC)
 
     return saved_tracks
+
+def get_saved_albums(sp_client: Spotify) -> dict:
+    # Can only get 50 albums at a time, iterate through the library until we've
+    # got them all
+    limit = 50
+    offset = 0
+    saved_albums = {}
+    while True:
+        results = sp_client.current_user_saved_albums(limit, offset)
+        result_items = results["items"]
+        print(f"Fetched {len(result_items)} albums")
+
+        # result_items is a list. Add them to the saved_albums dict by album ID
+        # to prevent duplicated albums appearing if the library was dded to
+        # while being fetched
+        for item in result_items:
+            saved_albums[item["album"]["id"]] = item
+        
+        if len(result_items) is not limit:
+            break
+        offset += limit
+
+        # Prevent rate limiting. Maybe not needed, playing it safe for now
+        time.sleep(API_REQUEST_SLEEP_TIME_SEC)
+    
+    return saved_albums
 
 
 def main():
@@ -47,6 +76,9 @@ def main():
     )
     saved_tracks = get_saved_tracks(sp_client)
     print(len(saved_tracks))
+
+    saved_albums = get_saved_albums(sp_client)
+    print(len(saved_albums))
 
 
 if __name__ == "__main__":
